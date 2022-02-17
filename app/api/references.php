88dbbe328a9701cef337dd6173cae594f418a394
Meilky <mikaeljean2k3@gmail.com>
2022-02-17 17:53:13 -0500

    Fix: French typos

--- app/api/references.php
+++ app/api/references.php
@@ -3,27 +3,27 @@
 $data = [
 	[
 		"name" => "Canvas 2d rendering context",
-		"description" => "Utiliser pour les references du context 2d des canvas javascript pour faire aparaitre les coupures.",
+		"description" => "Utilisé pour les références du context 2d des canvas javascript pour faire aparaitre les coupures.",
 		"link" => "https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D"
 	],
 	[
 		"name" => "React lifecycle",
-		"description" => "Utiliser pour baser le cycle de mes composent sur ceux des composent de React.",
+		"description" => "Utilisé pour baser le cycle de vie de mes \"custom component\" sur ceux de React.",
 		"link" => "https://www.w3schools.com/react/react_lifecycle.asp"
 	],
 	[
 		"name" => "Html element",
-		"description" => "Utiliser pour les reference au element html.",
+		"description" => "Utilisé pour les références au élément html.",
 		"link" => "https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement"
 	],
 	[
 		"name" => "Css flex box",
-		"description" => "Utiliser pour la nav bar.",
+		"description" => "Utilisé pour la nav bar, la page des références et la page d'accueil.",
 		"link" => "https://www.w3schools.com/css/css3_flexbox_container.asp"
 	],
 	[
 		"name" => "Html form event",
-		"description" => "Use to prevent the reload of the page when you submit your new preferences.",
+		"description" => "Utilisé pour que la page ne \"reload\" pas lorsque nous cliquons sur le boutton soumettre dans la page d'accueil.",
 		"link" => "https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event"
 	]
 ];
