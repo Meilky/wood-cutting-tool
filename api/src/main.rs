8ed83174520b6bdb1e0c9d20cb291f950f0daf2c
Meilky <mikaeljean2k3@gmail.com>
2022-03-29 15:15:53 -0400

    Fix bug in nginx

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder,middleware::Logger};
+use dotenv;
+use std::path::Path;
+
+async fn api() -> impl Responder {
+    HttpResponse::Ok().body("Hello from api!")
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::from_path(env_path).ok();
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/api", web::get().to(api))
+    })
+    .bind(("0.0.0.0", 4000))?
+    .run()
+    .await
+}
+
--- auth/src/main.rs
+++ auth/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use dotenv;
+use std::path::Path;
+
+
+async fn auth() -> impl Responder {
+    HttpResponse::Ok().body("Hello from auth!")
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let env_path = Path::new("/etc/wc-auth/.env");
+    dotenv::from_path(env_path).ok();
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/auth", web::get().to(auth))
+    })
+    .bind(("0.0.0.0", 4000))?
+    .run()
+    .await
+}
+
