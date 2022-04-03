2647595108befb7755df78dff14ed8f7cc08562a
Meilky <mikaeljean2k3@gmail.com>
2022-04-03 16:15:48 -0400

    Add endpoint to get only one reference

--- api/src/api/references/mod.rs
+++ api/src/api/references/mod.rs
@@ -4,4 +4,6 @@ mod urls;
 
 pub fn register_urls(cfg: &mut web::ServiceConfig) {
     cfg.route("", web::get().to(urls::get_references));
+    cfg.route("/", web::get().to(urls::get_references));
+    cfg.route("/{id}", web::get().to(urls::get_reference));
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -10,11 +10,11 @@ async fn main() -> std::io::Result<()> {
     let env_path = Path::new("/ect/wc-api/.env");
     dotenv::from_path(env_path).ok();
 
-    let pool = db::connection_builder().await.unwrap();
+    //let pool = db::connection_builder().await.unwrap();
 
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(pool.clone()))
+ //           .app_data(Data::new(pool.clone()))
             .configure(api::register_urls)
     })
     .bind(("0.0.0.0", 4000))?
