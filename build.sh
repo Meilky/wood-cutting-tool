f393afc4e2f3c20828e6579bc6aa9b63acfc63b2
Meilky <mikaeljean2k3@gmail.com>
2022-04-20 13:03:45 -0400

    CLean up

--- build.sh
+++ build.sh
@@ -3,13 +3,16 @@
 docker compose build
 
 cd ./ui
+npm run clean
 npm run build
 cd ..
 
 cd ./modules/references
+npm run clean
 npm run build
 cd ../..
 
 cd ./modules/wood-cutting-tool
+npm run clean
 npm run build
 cd ../..
